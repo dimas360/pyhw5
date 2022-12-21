9d1afff889cb2f71aa9c5e449930a6288a5a2005
dimas360 <bios5524@yandex.ru>
2022-12-21 16:30:20 +0300

    задача 2

---
+++ 2.py
@@ -0,0 +1,11 @@
+import math
+
+number=int(input("Введите число: "))
+
+for i in range(2, int(math.sqrt(number)) + 1): 
+    while (number % i == 0): 
+        print(i)
+        number //= i 
+
+if (number != 1): 
+    print (number)
